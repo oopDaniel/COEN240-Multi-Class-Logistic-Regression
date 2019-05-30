dbd2a06fea470100ca0411306da05aabd86e69da
Daniel <oopdaniel@gmail.com>
2019-05-29 18:37:40 -0700

    fin

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hand-written digit recognition using multi-class logistic regression
+
+- Simple program to recognize hand-written digits from mnist data using multi-class logistic regression
+- Plot the confusion matrix
+
+> Training
+
+![](./images/training.png)
+
+> Confusion matrix
+
+![](./images/confusion_matrix.png)
+
+
+## Get started
+
+> First time
+
+```bash
+make all
+```
+
+> After first time
+
+```bash
+make start
+```
+
+
+## Dependency:
+
+- Python 3+
+- numpy
+- tensorflow
+- sklearn
---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+if __name__ == '__main__':
+  # Load data
+  (train_data, train_label), (test_data, test_label) = mnist.load_data()
+
+  # Reshape data
+  train_data = np.reshape(train_data, (60000, 28 * 28))
+  test_data = np.reshape(test_data, (10000, 28 * 28))
+  train_data, test_data = train_data / 255.0, test_data / 255.0
+
+  # Build logistic regression model
+  log_reg = LogisticRegression(
+    solver='saga',
+    multi_class='multinomial',
+    max_iter=100,
+    verbose=2
+  )
+
+  # Train the model
+  log_reg.fit(train_data, train_label)
+
+  # Predict the result of test data
+  predition = log_reg.predict(test_data)
+
+  # Show the confusion matrix
+  print(confusion_matrix(test_label, predition))
